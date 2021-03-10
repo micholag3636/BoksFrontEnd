5f57b92973a9cec062fea0f78cdc2d2b869e606b
Mich <michola3636@gmail.com>
2021-03-10 19:05:30 +0000

    builds

--- src/components/Store.js
+++ src/components/Store.js
@@ -25,7 +25,7 @@ function Store() {
             <h2 className="storetitle">All Products</h2>
             <div className="store_products">
                 {loading ? (
-                  <div className="loader-box"><div class="loader"></div></div>
+                  <div className="loader-box"><div class="loaders"></div></div>
                 ): error ? (
                     <h2>{error}</h2>
                 ) : (
